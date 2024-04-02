8c007bdb31a4684cf4d0b79a243c5c6d31194bb1
IAmPaarsaa <154054226+IAmPaarsaa@users.noreply.github.com>
2024-04-02 23:40:08 +0330

    FIX: improve code's functionality as follows:
    - ask subdomains as a list from command line
    - validate passed arguments from command line
    - improve error handling of http requests
    - improve variable names and code structure
    - reduce time complexity  of the code

--- roles/prepare_webserver/files/arvancloud_api.py
+++ roles/prepare_webserver/files/arvancloud_api.py
@@ -1,69 +1,84 @@
 import requests
+import validators
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--wpdomain", type=str)
-parser.add_argument("--regdomain", type=str)
-parser.add_argument("--rootdomain", type=str)
-parser.add_argument("--apikey", type=str)
-parser.add_argument("--ansiblehost", type=str)
+parser.add_argument("--sub",type=str, nargs="*",required=True)
+parser.add_argument("--domain", type=str,required=True)
+parser.add_argument("--key", type=str,required=True)
+parser.add_argument("--ip", type=str,required=True)
 args = parser.parse_args()
 
-wp_domain = args.wpdomain
-reg_domain = args.regdomain
-root_domain = args.rootdomain
-api_key = args.apikey
-ansible_host = args.ansiblehost
 
-base_url = f"https://napi.arvancloud.ir/cdn/4.0/domains/{root_domain}/dns-records/"
+given_subdomains = args.sub
+domain = args.domain
+api_key = args.key
+ip_address = args.ip
+
+
+base_url = f"https://napi.arvancloud.ir/cdn/4.0/domains/{domain}/dns-records/"
 request_headers = {
     "Authorization": f"apikey {api_key}",
     "Accept": "application/json",
 }
 
+def validate_argsformat():
+    if not validators.ipv4(ip_address):
+        print("ERROR MESSAGE: Ip's Format Is Not Valid")
+        exit()
+    if not validators.domain(domain):
+        print("ERROR MESSAGE: Domain's Format Is Not Valid")
+        exit()
+    for each_subdomain in given_subdomains:
+        if not each_subdomain.isalpha():
+            print("ERROR MESSAGE: Subdomain's Format Is Not Valid")
+        # full_domain = each_subdomain + "." + domain
 
-def error_handling(response):
-    if response.status_code >= 400:
+
+def handle_errors(response):
+    if not response.ok:
         print("ERROR CODE:", response.status_code)
-        print("ERROR MESSAGE:", response.text)
+        print("ERROR MESSAGE:", response.reason)
         exit()
 
 
 def update_subdomain_records():
-    subdomain_list = [{"name": wp_domain, "id": ""}, {"name": reg_domain, "id": ""}]
+    validate_argsformat()
+    subdomain_list=[]
+    for each_given_subdomain in given_subdomains:
+        subdomain_list.append({"name": each_given_subdomain, "id":""})
 
-    get_response = requests.get(base_url, headers=request_headers, timeout=30)
-    error_handling(get_response)
+    list_response = requests.get(base_url, headers=request_headers, timeout=30)
+    handle_errors(list_response)
 
-    response_json = get_response.json()
+    response_json = list_response.json()
 
     record_dict = {}
     for record in response_json["data"]:
-        record_name = record["name"]
-        record_id = record["id"]
-        record_dict[record_name] = record_id
+        record_dict[record['name']] = record['id']
 
     for subdomain in subdomain_list:
         subdomain["id"] = record_dict.get(subdomain["name"])
-
-        record_url = base_url + subdomain["id"]
         record_payload = {
-            "value": [{"ip": ansible_host}],
+            "value": [{"ip": ip_address}],
             "type": "a",
             "name": subdomain["name"],
             "ttl": 120,
             "cloud": False,
         }
+
         if not subdomain["id"]:
-            post_response = requests.post(
+            add_response = requests.post(
                 url=base_url, headers=request_headers, json=record_payload, timeout=30
             )
-            error_handling(post_response)
-            break
-        put_response = requests.put(
+            handle_errors(add_response.json)
+            continue
+
+        record_url = base_url + subdomain["id"]
+        update_response = requests.put(
             url=record_url, json=record_payload, headers=request_headers, timeout=30
         )
-        error_handling(put_response)
+        handle_errors(update_response)
 
 
 update_subdomain_records()
