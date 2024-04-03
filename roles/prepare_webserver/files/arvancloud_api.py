5be4e4210ea9ffe1ae4b389cf994dd5b9ad56664
Hossein Taleghani <hossein@arvancloud.com>
2024-04-03 20:57:01 +0330

    FIX: replace global variables with local vars

--- roles/prepare_webserver/files/arvancloud_api.py
+++ roles/prepare_webserver/files/arvancloud_api.py
@@ -3,44 +3,30 @@ import requests
 import validators
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--subdomain", type=str, nargs="*", required=True)
-parser.add_argument("--domain", type=str, required=True)
-parser.add_argument("--apikey", type=str, required=True)
-parser.add_argument("--ip", type=str, required=True)
-args = parser.parse_args()
 
-
-subdomains_list = args.subdomain
-domain = args.domain
-api_key = args.apikey
-ip_address = args.ip
-
-
-def validate_argsformat():
-    if not validators.ipv4(ip_address):
+def is_args_valid(domain: str, subdomains: list[str], ip: str) -> bool:
+    if not validators.ipv4(ip):
         print("ERROR MESSAGE: Ip's Format Is Not Valid")
         return False
     if not validators.domain(domain):
         print("ERROR MESSAGE: Domain's Format Is Not Valid")
         return False
-    for subdomain in subdomains_list:
+    for subdomain in subdomains:
         if not subdomain.isalnum():
             print("ERROR MESSAGE: Subdomain's Format Is Not Valid")
             return False
     return True
 
 
-def handle_response(response):
+def exit_on_errors(response):
     if not response.ok:
         print("ERROR CODE:", response.status_code)
         print("ERROR MESSAGE:", response.json()["message"])
-        return False
-    return True
+        exit()
 
-def update_subdomain_records():
-    validation_result = validate_argsformat()
-    if not validation_result:
+
+def update_subdomain_records(domain: str, subdomains: list[str], ip: str, api_key: str):
+    if not is_args_valid(domain, subdomains, ip):
         return 0
 
     base_url = f"https://napi.arvancloud.ir/cdn/4.0/domains/{domain}/dns-records/"
@@ -48,41 +34,48 @@ def update_subdomain_records():
         "Authorization": f"apikey {api_key}",
         "Accept": "application/json",
     }
-    subdomains_dict = defaultdict(str)
 
     list_response = requests.get(base_url, headers=request_headers, timeout=30)
-    request_result = handle_response(list_response)
-    if not request_result:
-        return 0
 
-    record_list = list_response.json()
+    exit_on_errors(list_response)
 
-    for record in record_list["data"]:
-        subdomains_dict[record["name"]] = record["id"]
+    records = list_response.json()
+    records_by_name = defaultdict(str)
 
-    for subdomain in subdomains_list:
+    for record in records["data"]:
+        records_by_name[record["name"]] = record["id"]
+
+    for subdomain in subdomains:
         record_payload = {
-            "value": [{"ip": ip_address}],
+            "value": [{"ip": ip}],
             "type": "a",
             "name": subdomain,
             "ttl": 120,
             "cloud": False,
         }
 
-        subdomain_id = subdomains_dict[subdomain]
+        record_id = records_by_name[subdomain]
 
-        if not subdomain_id:
+        if not record_id:
             add_response = requests.post(
                 url=base_url, headers=request_headers, json=record_payload, timeout=30
             )
-            handle_response(add_response)
+            exit_on_errors(add_response)
             continue
 
-        record_url = base_url + subdomain_id
+        record_url = base_url + record_id
         update_response = requests.put(
             url=record_url, json=record_payload, headers=request_headers, timeout=30
         )
-        handle_response(update_response)
+        exit_on_errors(update_response)
 
 
-update_subdomain_records()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--subdomain", type=str, nargs="*", required=True)
+    parser.add_argument("--domain", type=str, required=True)
+    parser.add_argument("--apikey", type=str, required=True)
+    parser.add_argument("--ip", type=str, required=True)
+
+    args = parser.parse_args()
+    update_subdomain_records(args.domain, args.subdomain, args.ip, args.apikey)
